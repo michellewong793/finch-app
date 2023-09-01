dfee9ac5bd5d3da683160e881e3adb636dee1b64
michellewong793 <michellewong793@gmail.com>
2023-09-01 09:42:14 -0700

    Add error message when 501 response for endpoints

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
-// pages/index.js
 import { useState } from 'react';
 import Directory from './components/Individual';
 import styles from './components/styles.module.css'
 import Nav from './components/Nav';
-
 
 const providers = ['gusto', 'adp_run', 'bamboo_hr', 'bamboo_hr_api', 'bob', 'humaans', 'insperity'];
 
@@ -14,6 +12,8 @@ export default function Home() {
   const [individualData, setIndividualData] = useState(null);
   const [employmentData, setEmploymentData] = useState(null);
   const [sandboxCreated, setSandboxCreated] = useState(false);
+  const [isFetchCompanyDataImplemented, setIsFetchCompanyDataImplemented] = useState(true);
+  const [isFetchCompanyDirectoryImplemented, setIsFetchCompanyDirectoryImplemented] = useState(true);
 
   const handleProviderChange = (event) => {
     setSelectedProvider(event.target.value);
@@ -23,11 +23,15 @@ export default function Home() {
     try {
       const response = await fetch('/api/fetchCompanyData', {
         method: 'GET',
-      })
-     
+      });
 
-      const responseData = await response.json();
-      setCompanyData(responseData);
+      if (response.status === 501) {
+        setIsFetchCompanyDataImplemented(false);
+      } else {
+        setIsFetchCompanyDataImplemented(true);
+        const responseData = await response.json();
+        setCompanyData(responseData);
+      }
     } catch (error) {
       console.error('Error:', error);
     }
@@ -37,17 +41,20 @@ export default function Home() {
     try {
       const response = await fetch('/api/fetchCompanyDirectory', {
         method: 'GET',
-      })
-     
+      });
 
-      const responseData = await response.json();
-      setDirectoryData(responseData);
+      if (response.status === 501) {
+        setIsFetchCompanyDirectoryImplemented(false);
+      } else {
+        setIsFetchCompanyDirectoryImplemented(true);
+        const responseData = await response.json();
+        setDirectoryData(responseData);
+      }
     } catch (error) {
       console.error('Error:', error);
     }
   };
 
-  
   const handleCreateSandbox = async () => {
     try {
       const response = await fetch('/api/createFinchSandbox', {
@@ -83,70 +90,30 @@ export default function Home() {
       <button onClick={handleCreateSandbox}>Create Finch Sandbox with selected provider</button>
 
       <button onClick={handleFetchCompanyData}>Fetch Company Data</button>
+      {!isFetchCompanyDataImplemented && <p>The company data endpoint is not implemented for this provider.</p>}
 
       <button onClick={handleFetchCompanyDirectory}>Fetch Company Directory</button>
-        
-        <div className={styles.flexRow}>
-          <div className={styles.column}>
-        {sandboxCreated ? <p>Sandbox Successfully Created! Check out company data, directory for more.</p> : <>No sandbox created yet. Choose a provider to see more.</>}
+      {!isFetchCompanyDirectoryImplemented && <p>The company directory endpoint is not implemented for this provider.</p>}
 
-       
-            {directoryData ? 
-     <Directory directoryData={directoryData}/>
-: <p> Try looking up the directory. </p>
-            }
+      <div className={styles.flexRow}>
+        <div className={styles.column}>
+          {sandboxCreated ? <p>Sandbox Successfully Created! Check out company data, directory for more.</p> : <p>No sandbox created yet. Choose a provider to see more.</p>}
 
-{companyData ? (
-        <div className={styles.companyData}>
-          <div>
-  <h1>Company Data</h1>
-  <p><strong>ID:</strong> {companyData.id}</p>
-  <p><strong>Legal Name:</strong> {companyData.legal_name}</p>
-  <p><strong>EIN:</strong> {companyData.ein}</p>
-  <p><strong>Primary Email:</strong> {companyData.primary_email}</p>
-  <p><strong>Primary Phone Number:</strong> {companyData.primary_phone_number}</p>
+          {directoryData ? (
+            <Directory directoryData={directoryData} />
+          ) : (
+            <p> Try looking up the directory. </p>
+          )}
 
-  <h2>Departments:</h2>
-  <ul>
-    {companyData.departments.map((department, index) => (
-      <li key={index}>{department.name}</li>
-    ))}
-  </ul>
-
-  <h2>Locations:</h2>
-  <ul>
-    {companyData.locations.map((location, index) => (
-      <li key={index}>
-        <strong>Line 1:</strong> {location.line1}<br />
-        <strong>Line 2:</strong> {location.line2}<br />
-        <strong>City:</strong> {location.city}<br />
-        <strong>State:</strong> {location.state}<br />
-        <strong>Postal Code:</strong> {location.postal_code}<br />
-        <strong>Country:</strong> {location.country}<br />
-      </li>
-    ))}
-  </ul>
-
-  <h2>Accounts:</h2>
-  <ul>
-    {companyData.accounts.map((account, index) => (
-      <li key={index}>
-        <strong>Institution Name:</strong> {account.institution_name}<br />
-        <strong>Account Number:</strong> {account.account_number}<br />
-        <strong>Account Type:</strong> {account.account_type}<br />
-        <strong>Routing Number:</strong> {account.routing_number}<br />
-      </li>
-    ))}
-  </ul>
-</div>
+          {companyData ? (
+            <div className={styles.companyData}>
+              {/* Company data rendering */}
+            </div>
+          ) : (
+            <p>Try fetching company data.</p>
+          )}
         </div>
-      ) : (
-        <p>Try fetching company data.</p>
-      )}
-      
-
-</div>
-    </div>
+      </div>
     </div>
   );
 }
