029b29cc6953f4b7541a114575eda570e57cad41
michellewong793 <michellewong793@gmail.com>
2023-09-04 12:14:11 -0700

    delete console.log

--- pages/api/employment.js
+++ pages/api/employment.js
@@ -30,8 +30,6 @@ export default async function handler(req, res) {
       });
 
       const responseData = await response.json();
-      console.log("employment", responseData)
-
       res.status(response.status).json(responseData);
     } catch (error) {
       console.error('Error:', error);
