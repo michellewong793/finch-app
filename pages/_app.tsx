49a0982cdb976514465e09d7322b3e5347d23f72
michellewong793 <michellewong793@gmail.com>
2023-09-04 12:34:02 -0700

    Add types

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <main className={inter.className}>
       <Component {...pageProps} />
