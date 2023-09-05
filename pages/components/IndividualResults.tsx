d8f638f3185c778527c23908b97a95430eafa8fc
michellewong793 <michellewong793@gmail.com>
2023-09-04 21:32:32 -0700

    add empty value check for emails

--- pages/components/IndividualResults.tsx
+++ pages/components/IndividualResults.tsx
@@ -10,74 +10,83 @@ interface ContactInfo {
 
 // Define an interface for the shape of an individual's residence
 interface Residence {
-  line1: string;
-  line2: string;
-  city: string;
-  state: string;
-  postal_code: string;
-  country: string;
+  line1: string | null; // Update to accept null values
+  line2: string | null; // Update to accept null values
+  city: string | null; // Update to accept null values
+  state: string | null; // Update to accept null values
+  postal_code: string | null; // Update to accept null values
+  country: string | null; // Update to accept null values
 }
 
 // Define an interface for the shape of an individual's response
 interface IndividualResponseData {
   id: string;
-  first_name: string;
-  middle_name: string;
-  last_name: string;
-  dob: string;
-  gender: string;
-  emails: ContactInfo[];
-  phone_numbers: ContactInfo[];
+  first_name: string | null; // Update to accept null values
+  middle_name: string | null; // Update to accept null values
+  last_name: string | null; // Update to accept null values
+  dob: string | null; // Update to accept null values
+  gender: string | null; // Update to accept null values
+  emails: (ContactInfo | null)[]; // Update to accept null values
+  phone_numbers: (ContactInfo | null)[]; // Update to accept null values
   residence: Residence;
 }
 
 // Define the props interface for the IndividualResponse component
 interface IndividualResponseProps {
   response: {
-    body: IndividualResponseData;
+    body: IndividualResponseData | null; // Update to accept null values
   };
 }
 
 function IndividualResponse({ response }: IndividualResponseProps) {
   const individual = response.body;
 
+  if (!individual) {
+    return <p>N/A</p>;
+  }
+
   return (
     <div className={styles.individualResults}>
       <h2>Individual Information</h2>
       <p><strong>ID:</strong> {individual.id}</p>
-      <p><strong>First Name:</strong> {individual.first_name}</p>
-      <p><strong>Middle Name:</strong> {individual.middle_name}</p>
-      <p><strong>Last Name:</strong> {individual.last_name}</p>
-      <p><strong>Date of Birth:</strong> {individual.dob}</p>
-      <p><strong>Gender:</strong> {individual.gender}</p>
+      <p><strong>First Name:</strong> {individual.first_name || "N/A"}</p>
+      <p><strong>Middle Name:</strong> {individual.middle_name || "N/A"}</p>
+      <p><strong>Last Name:</strong> {individual.last_name || "N/A"}</p>
+      <p><strong>Date of Birth:</strong> {individual.dob || "N/A"}</p>
+      <p><strong>Gender:</strong> {individual.gender || "N/A"}</p>
 
       <h3>Emails</h3>
-      <ul>
-        {individual.emails.map((email, index) => (
-          <li key={index}>{email.data} ({email.type})</li>
-        ))}
-      </ul>
+      {individual.emails.length > 0 ? (
+        <ul>
+          {individual.emails.map((email, index) => (
+            <li key={index}>{email ? `${email.data} (${email.type})` : "N/A"}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>N/A</p>
+      )}
+
 
       <h3>Phone Numbers</h3>
       <ul>
         {individual.phone_numbers.map((phone, index) => (
-          <li key={index}>{phone.data} ({phone.type})</li>
+          <li key={index}>{phone ? `${phone.data} (${phone.type})` : "N/A"}</li>
         ))}
       </ul>
 
       <h3>Residence</h3>
-      <p><strong>Address:</strong> {individual.residence.line1}, {individual.residence.line2}</p>
-      <p><strong>City:</strong> {individual.residence.city}</p>
-      <p><strong>State:</strong> {individual.residence.state}</p>
-      <p><strong>Postal Code:</strong> {individual.residence.postal_code}</p>
-      <p><strong>Country:</strong> {individual.residence.country}</p>
+      <p><strong>Address:</strong> {individual.residence.line1 || "N/A"}, {individual.residence.line2 || "N/A"}</p>
+      <p><strong>City:</strong> {individual.residence.city || "N/A"}</p>
+      <p><strong>State:</strong> {individual.residence.state || "N/A"}</p>
+      <p><strong>Postal Code:</strong> {individual.residence.postal_code || "N/A"}</p>
+      <p><strong>Country:</strong> {individual.residence.country || "N/A"}</p>
     </div>
   );
 }
 
 interface IndividualResponseListProps {
   responses?: {
-    body: IndividualResponseData;
+    body: IndividualResponseData | null; // Update to accept null values
   }[];
 }
 
@@ -86,7 +95,7 @@ function IndividualResponseList({ responses }: IndividualResponseListProps) {
     <div>
       {responses && responses.length > 0 ? (
         responses.map((response, index) => (
-          <IndividualResponse key={index} response={response} />
+          <IndividualResponse key={index} response={response || { body: null }} />
         ))
       ) : (
         <p>No individual responses available.</p>
